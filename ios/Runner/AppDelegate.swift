aa0e62b210fef484702c63630f394b76e9f9baf3
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-04-05 10:40:40 +0900

    🎨 [ios]format code swift
    swiftlint autocorrect

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,31 +10,31 @@ import Flutter
         // MethodChannel
         demoMethodChannel1()
         demoMethodChannel2()
-        
+
         // MessageChannel
         demoBasicMessageChannel1()
         demoBasicMessageChannel2()
         demoBasicMessageChannel3()
-        
+
         // EventChannel
         demoEventChannel()
-        
+
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    
+
     // MARK: MethodChannel
     func demoMethodChannel1() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let methodChannel = FlutterMethodChannel(name: "com.flutter/method1", binaryMessenger: controller.binaryMessenger)
-        
+
         methodChannel.setMethodCallHandler { (call, result) in
             if call.method == "getDeviceInfoString" {
-                if let atgs = call.arguments as? Dictionary<String, Any>, let type = atgs["type"] as? String {
+                if let atgs = call.arguments as? [String: Any], let type = atgs["type"] as? String {
                     if type == "MODEL" {
                         result(UIDevice.current.name)
-                        //var json = [String: Any]()
-                        //result(json) // StandarMethodCodec
+                        // var json = [String: Any]()
+                        // result(json) // StandarMethodCodec
                     } else {
                         result("unknow")
                     }
@@ -45,14 +45,14 @@ import Flutter
             result(FlutterMethodNotImplemented)
         }
     }
-    
+
     func demoMethodChannel2() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let methodChannel = FlutterMethodChannel(name: "com.flutter/method2", binaryMessenger: controller.binaryMessenger, codec: FlutterJSONMethodCodec.sharedInstance())
-        
+
         methodChannel.setMethodCallHandler { (call, result) in
             if call.method == "getDeviceInfo" {
-                if let atgs = call.arguments as? Dictionary<String, Any>, let type = atgs["type"] as? String {
+                if let atgs = call.arguments as? [String: Any], let type = atgs["type"] as? String {
                     if type == "MODEL" {
                         var json = [String: Any]()
                         json["name"] = UIDevice.current.name
@@ -71,23 +71,23 @@ import Flutter
             result(FlutterMethodNotImplemented)
         }
     }
-    
+
     // MARK: BasicMessageChannel
     func demoBasicMessageChannel1() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let messageChannel = FlutterBasicMessageChannel(name: "StringCodec", binaryMessenger: controller.binaryMessenger, codec: FlutterStringCodec.sharedInstance())
-        
+
         messageChannel.setMessageHandler { (message, reply) in
             messageChannel.sendMessage("Hello \(String(describing: message!)) from native code")
             reply(nil)
         }
     }
-    
+
     func demoBasicMessageChannel2() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let messageChannel = FlutterBasicMessageChannel(name: "JSONMessageCodec", binaryMessenger: controller.binaryMessenger, codec: FlutterJSONMessageCodec.sharedInstance())
-        
-        messageChannel.setMessageHandler { (message, reply) in
+
+        messageChannel.setMessageHandler { (_, reply) in
             var json = [String: Any]()
             json["name"] = UIDevice.current.name
             json["batteryLevel"] = UIDevice.current.batteryLevel
@@ -98,22 +98,22 @@ import Flutter
             reply(nil)
         }
     }
-    
+
     func demoBasicMessageChannel3() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let messageChannel = FlutterBasicMessageChannel(name: "BinaryCodec", binaryMessenger: controller.binaryMessenger, codec: FlutterBinaryCodec.sharedInstance())
-        
-        messageChannel.setMessageHandler { (message, reply) in
+
+        messageChannel.setMessageHandler { (_, _) in
            // searching
-            
+
         }
     }
-    
+
     // MARK: EventChannel
     func demoEventChannel() {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let event = FlutterEventChannel(name: "stream", binaryMessenger: controller.binaryMessenger)
-        
+
         event.setStreamHandler(SwiftStreamHandler())
     }
 }
@@ -122,27 +122,27 @@ import Flutter
 // https://stackoverflow.com/a/26823723/6284714
 // https://stackoverflow.com/a/38696111/6284714
 class SwiftStreamHandler: NSObject, FlutterStreamHandler {
-    var count:Int = 0
+    var count: Int = 0
     var timer: Timer?
     var eventSink: FlutterEventSink?
-    
+
     @objc func onTick() {
         self.count += 1
         if let event = self.eventSink {
             event(String(self.count))
         }
     }
-    
+
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         self.eventSink = events
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(onTick), userInfo: nil, repeats: true)
         return nil
     }
-    
+
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         self.timer?.invalidate()
         self.timer = nil
         return nil
     }
-    
+
 }
